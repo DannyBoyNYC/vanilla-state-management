7196e501f194fe77e76e6800cc67a21f2def41c9
DannyBoyNYC <daniel.deverell@gmail.com>
2019-09-23 08:07:01 -0400

    state based ui

--- index.js
+++ index.js
@@ -1,37 +1,17 @@
-document.addEventListener(
-  'submit',
-  function(event) {
-    // Make sure the submitted form was for our list items
-    if (!event.target.matches('#add-to-list')) return;
+// Data State
+var data = {
+  listItems: [],
+};
 
-    // Stop the form from submitting
-    event.preventDefault();
-
-    // Get the list item
-    var item = event.target.querySelector('#list-item');
-    // console.log(item);
-    if (!item || item.length < 1) return;
-
-    // Update the data and UI
-    data.listItems.push(item.value);
-    render();
-
-    // Clear the field and return to focus
-    item.value = '';
-    item.focus();
-  },
-  false,
-);
-
+// UI Template
 var template = function() {
   // If there are no list items
-  if (template.data.listItems.length < 1)
+  if (data.listItems.length < 1)
     return '<p><em>You do not have any list items yet. Try adding one with the form above.</em></p>';
-
   // If there are
   return (
     '<ul>' +
-    template.data.listItems
+    data.listItems
       .map(function(item) {
         return '<li>' + item + '</li>';
       })
@@ -40,37 +20,33 @@ var template = function() {
   );
 };
 
-var getData = function() {
-  return JSON.parse(JSON.stringify(template.data));
-};
-
-// Reactively update data
-var setData = function(obj) {
-  for (var key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      template.data[key] = obj[key];
-    }
-  }
-  render();
-};
-
+// Function to render the UI into the DOM
 var render = function() {
   var list = document.querySelector('#list');
   if (!list) return;
   list.innerHTML = template();
 };
 
-setData({ listItems: ['Dumbledore', 'Hermione', 'Gandalf', 'Neville'] });
-
+// Render the UI
 render();
 
-template.data = {
-  listItems: [],
-};
-
-// // new var, new state
-// var items = getData();
-// console.log(items);
-// items.listItems.push(item.value);
-// // console.log(items);
-// setData({ listItems: items.listItems });
+// Listen for form submissions
+document.addEventListener(
+  'submit',
+  function(event) {
+    // Make sure the submitted form was for our list items
+    if (!event.target.matches('#add-to-list')) return;
+    // Stop the form from submitting
+    event.preventDefault();
+    // Get the list item
+    var item = event.target.querySelector('#list-item');
+    if (!item || item.length < 1) return;
+    // Update the data and UI
+    data.listItems.push(item.value);
+    render();
+    // Clear the field and return to focus
+    item.value = '';
+    item.focus();
+  },
+  false,
+);
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 https://gomakethings.com/products/web-apps-FnXdW76c/web-apps.html
+
+https://github.com/cferdinandi/web-apps-source-code/
