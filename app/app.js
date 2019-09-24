eca5b6791d5c769794a4d47cd6e49fc095e1db70
DannyBoyNYC <daniel.deverell@gmail.com>
2019-09-23 22:45:44 -0400

    done

--- app/app.js
+++ app/app.js
@@ -1,86 +1,198 @@
-var getParams = function(url) {
-  var params = {};
-  var parser = document.createElement('a');
-  parser.href = url || window.location.href;
-  var query = parser.search.substring(1);
-  var vars = query.split('&');
-  if (vars.length < 1) return params;
-  for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split('=');
-    params[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
-  }
-  return params;
-};
-
-// Create a new list component
-var lists = new Component('#app', {
+/**
+ * Setup the new app component
+ */
+var app = new Component('#app', {
   data: {
-    listItems: []
+    drivers: [],
+    selected: null,
   },
   template: function(props) {
-    var baseHTML = `<p><a href="?page=settings">Settings &rarr;</a></p>
-      <h1>List Maker</h1>
-      <form id="add-to-list">
-      <label for="list-item">What do you want to add to your list?</label>
-      <input type="text" id="list-item" autofocus>
-      <button>Add to your list</button>
-      </form>`;
+    // Save data to localStorage
+    localStorage.setItem('drivers', JSON.stringify(props.drivers));
 
-    // If there are no list items
-    if (props.listItems.length < 1)
-      return `${baseHTML}
-        <p><em>You do not have any list items yet. Try adding one with the form above.</em></p>`;
-    // If there are
-    return `${baseHTML}
-      <ul>
-      ${props.listItems
-        .map(item => {
-          return `<li>${item}</li>`;
+    // If there's no driver yet
+    if (props.drivers.length < 1) {
+      return '<p><em>Add a driver or two below to get started.</em></p>';
+    }
+
+    // If there's a selected driver
+    var selected = '';
+    if (props.selected) {
+      selected =
+        '<p><strong>' +
+        sanitizeHTML(props.selected) +
+        ' is driving.</strong></p>';
+    }
+
+    // Create the list of drivers
+    return (
+      '<ul>' +
+      props.drivers
+        .map(function(driver, index) {
+          var name = sanitizeHTML(driver);
+          return (
+            '<li>' +
+            name +
+            ' <button aria-label="Remove ' +
+            name +
+            '" data-driver="' +
+            index +
+            '">remove</button></li>'
+          );
         })
-        .join('')}
-      </ul>`;
-  }
+        .join('') +
+      '</ul>' +
+      selected
+    );
+  },
 });
 
-// Create a settings component
-var settings = new Component('#app', {
-  template: function() {
-    var html = `<p><a href="index.html">&larr; Back to Lists</a></p>
-      <h1>Settings</h1>
-      <p>A settings UI would go here.</p>`;
-    return html;
-  }
-});
-
-var submitHandler = function(event) {
-  // Make sure the submitted form was for our list items
-  if (!event.target.matches('#add-to-list')) return;
-
-  // Stop the form from submitting
-  event.preventDefault();
-
-  // Get the list item
-  var item = event.target.querySelector('#list-item');
-  if (!item || item.length < 1) return;
-
-  // Reactively update the data
-  var items = lists.getData();
-  items.listItems.push(item.value);
-  lists.setData({ listItems: items.listItems });
-
-  // Clear the field and return to focus
-  item = document.querySelector('#list-item');
-  if (!item) return;
-  item.focus();
+/*!
+ * Sanitize and encode all HTML in a user-submitted string
+ * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {String} str  The user-submitted string
+ * @return {String} str  The sanitized string
+ */
+var sanitizeHTML = function(str) {
+  var temp = document.createElement('div');
+  temp.textContent = str;
+  return temp.innerHTML;
 };
 
-// Determine the view/UI
-var page = getParams();
-if (page['page'] === 'settings') {
-  settings.render();
+/**
+ * Handle form submit events
+ */
+var submitHandler = function(event) {
+  // Check if the form is the #add-driver form
+  if (event.target.id !== 'add-driver') return;
+
+  // Prevent form from submitting to the server
+  event.preventDefault();
+
+  // Make sure a name was provided
+  var input = event.target.querySelector('#driver-name');
+  if (!input || input.value.length < 1) return;
+
+  // Update the data state
+  var data = app.getData();
+  data.drivers.push(input.value);
+  app.setData({ drivers: data.drivers });
+
+  // Clear input and return to focus
+  input.value = '';
+  input.focus();
+};
+
+/**
+ * Pick someone to drive
+ */
+var pickDriver = function() {
+  // Get the data
+  var data = app.getData();
+
+  // If there are no drivers, alert a message and quit
+  if (data.drivers.length < 2) {
+    alert('Oops! You need to add at least two drivers first.');
+    return;
+  }
+
+  // Shuffle the drivers and select a driver
+  shuffle(data.drivers);
+  var selected = data.drivers[0];
+  if (data.selected && selected === data.selected) {
+    selected = data.drivers[1];
+  }
+
+  // Update the state and UI
+  app.setData({ selected: selected });
+};
+
+/**
+ * Randomly shuffle an array
+ * https://stackoverflow.com/a/2450976/1293256
+ * @param  {Array} array The array to shuffle
+ * @return {String}      The first item in the shuffled array
+ */
+var shuffle = function(array) {
+  var currentIndex = array.length;
+  var temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+};
+
+/**
+ * Handle click events
+ */
+var clickHandler = function(event) {
+  // If pick a driver button
+  if (event.target.id === 'pick-driver') {
+    pickDriver();
+    return;
+  }
+
+  // If remove driver button
+  var driver = event.target.getAttribute('data-driver');
+  if (driver) {
+    removeDriver(driver);
+    return;
+  }
+
+  // If clear all button
+  if (event.target.id === 'clear-drivers') {
+    clearDrivers();
+  }
+};
+
+/**
+ * Remove a driver from the list
+ * @param  {Number} driver The index of the driver in the data state
+ */
+var removeDriver = function(driver) {
+  // Get immutable copy of the data
+  var data = app.getData();
+
+  // Remove the driver
+  data.drivers.splice(driver, 1);
+
+  // Update the state and UI
+  app.setData({ drivers: data.drivers });
+};
+
+/**
+ * Remove all drivers from the list
+ */
+var clearDrivers = function() {
+  // Confirm the user wants to do this first
+  if (!confirm('Are you sure you want to remove all drivers?')) return;
+
+  // Update the data and UI
+  app.setData({
+    drivers: [],
+    selected: null,
+  });
+};
+
+// If drivers saved to localStorage, get them
+// Otherwise, render with default
+var saved = localStorage.getItem('drivers');
+if (saved) {
+  app.setData({ drivers: JSON.parse(saved) });
 } else {
-  // Render the initial UI
-  lists.render();
-  // Listen for form submissions
-  document.addEventListener('submit', submitHandler, false);
+  app.render();
 }
+
+/// Event Listeners
+document.addEventListener('submit', submitHandler, false);
+document.addEventListener('click', clickHandler, false);
